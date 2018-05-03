dfc9e162629feef6c70fc3366fcc7da243bb0845
张仕昊 <Pandc@zhangshihaodemacbook-pro.local>
2018-05-03 15:33:44 +0800

    自定义背景颜色，导航栏，导航栏颜色，标题

--- MyFirstApp/ViewController.m
+++ MyFirstApp/ViewController.m
@@ -17,6 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIColor *gray = [UIColor colorWithRed:0.95 green:0.95 blue:0.95 alpha:1];
+    self.view.backgroundColor = gray;
+    
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:0.2 green:0.72 blue:0.46 alpha:1];
+    self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
+    
+    
+    UILabel *titleLable = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 150, 44)];
+    titleLable.text = @"2015年9月";
+    titleLable.textColor = [UIColor whiteColor];
+    titleLable.font = [UIFont boldSystemFontOfSize:17];
+    titleLable.textAlignment = NSTextAlignmentCenter;
+    titleLable.backgroundColor = [UIColor clearColor];
+    
+    self.navigationItem.titleView = titleLable;
+    
 }
 
 
